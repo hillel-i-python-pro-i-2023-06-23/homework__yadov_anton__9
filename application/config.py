160fec88fcbf0b25295e4a2d0c1b710f880a6e36
Anton <antonyadoval@gmail.com>
2023-09-22 16:07:30 +0300

    1

--- application/config.py
+++
@@ -1,6 +0,0 @@
-import pathlib
-from typing import Final
-
-ROOT_DIR: Final[pathlib.Path] = pathlib.Path(__file__).parents[1]
-FILES_OUTPUT_DIR: Final[pathlib.Path] = ROOT_DIR.joinpath("files_output")
-DB_PATH: Final[pathlib.Path] = ROOT_DIR.joinpath("db", "db.sqlite")
